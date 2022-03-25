90c0cfc382ae70ed197188e4f751c974dad5a75a
Rachappa-Minajagi <m.rachoti142@gmail.com>
2022-03-25 19:34:45 +0530

    Added:Multiple Rides

--- src/main/java/cabInvoiceGenerator/CabInvoiceGenerator.java
+++ src/main/java/cabInvoiceGenerator/CabInvoiceGenerator.java
@@ -2,28 +2,24 @@
  * Step1:-Given Distance and time,the invoice generator should return the total fare journey
  * Cost- RS 10 Per Kilometer + 1 Rs.Per minute
  * Minimum Fare -RS.5
+ * 
+ *Step2:-The invoice generate should now take multiple rides,and calculate aggregate total for all.
  */
 
 package cabInvoiceGenerator;
 
-/**
- * Create class CabInvoiceGenerator
- * 
- * @author Rachoti
- *
- */
+import org.junit.Rule;
 
 public class CabInvoiceGenerator {
 	/**
 	 * final keyword fix the value does not change
 	 */
-	final int COST_PER_KM = 10; // 10 Rs
-	final int COST_PER_MIN = 1;// 1 RS
+	final int COST_PER_KM = 10;
+	final int COST_PER_MIN = 1;
 	final int MIN_FARE = 5;
 
 	public static void main(String[] args) {
 		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
-
 		System.out.println(cabInvoiceGenerator.calculateFare(5, 20) + " Rs");
 	}
 
@@ -33,14 +29,19 @@ public class CabInvoiceGenerator {
 		 */
 		double fare = COST_PER_KM * distance + COST_PER_MIN * time;
 		if (fare > MIN_FARE)
-			/**
-			 * return total Minimum fare
-			 */
 			return fare;
 		else
-			/**
-			 * return total Maximum fare
-			 */
 			return MIN_FARE;
+	}
+
+	public double calculateFareForMultipleRides(Rule[] ridesArray) {
+		double totalFare = 0;
+		for (Rule ride : ridesArray) {
+			totalFare += calculateFare(ride.DEFAULT_ORDER, ride.DEFAULT_ORDER);
+		}
+		/**
+		 * return total Minimum fare
+		 */
+		return totalFare;
 	}
 }
--- src/test/java/CabInvoiceGeneratorTest.java
+++ src/test/java/CabInvoiceGeneratorTest.java
@@ -7,14 +7,19 @@ import org.junit.Test;
 
 import cabInvoiceGenerator.CabInvoiceGenerator;
 
-/**
- * Create class CabInvoiceGeneratorTest
- */
 public class CabInvoiceGeneratorTest {
 
 	@Test
 	public void whenGivenDistanceAndTimeShouldReturnTotalFare() {
 		CabInvoiceGenerator fare = new CabInvoiceGenerator();
 		Assert.assertEquals(70, fare.calculateFare(5, 20), 0.000);
+	}
+
+	@Test
+	public void givenMultipleRides_ShouldReturnTotalOfTotalFare() {
+		Ride[] rides = { new Ride((int) 2.0, 5), new Ride((int) 5.0, 10), new Ride((int) 0.1, 1), new Ride(20, 60) };
+		CabInvoiceGenerator invoiceService = new CabInvoiceGenerator();
+		double totalFare = (Double) ((Object) invoiceService);
+		Assert.assertEquals(260, totalFare, 0);
 	}
 }
